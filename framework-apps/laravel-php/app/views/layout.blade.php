aed1fbca702004851af7622fb392a0b400412268
Joridos <jonatam.ribeiro@hotmail.com>
2014-12-14 17:05:26 -0200

    added laravel framework

---
+++ framework-apps/laravel-php/app/views/layout.blade.php
@@ -0,0 +1,6 @@
+<html>
+  <body>
+    <h1>Morse code converter in Laravel</h1>
+    @yield('content')
+  </body>
+</html>
---
+++ framework-apps/laravel-php/app/views/result.blade.php
@@ -0,0 +1,7 @@
+
+@extends('layout')
+
+@section('content')
+    The morse code is: <b>{{$code}}</b><br>
+    <a href="/">Convert new Message</a>
+@stop
